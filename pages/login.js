87d9f3d88bb65c9eeeb6b7cb2fce96e7fb0a9a22
parvin.pashazada <parvin.pashazada@a2z.az>
2022-03-25 02:44:02 +0400

    fixes

--- pages/login.js
+++ pages/login.js
@@ -14,6 +14,7 @@ import {
     getAuth,
     GoogleAuthProvider,
     signInWithPopup,
+    // createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     FacebookAuthProvider,
 } from "firebase/auth";
--- pages/signup.js
+++ pages/signup.js
@@ -5,8 +5,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import config from "../src/configuration";
 import Image from "next/image";
-import { useDispatch } from "react-redux";
-import { setCookie } from "../src/helpers/cookie";
+import { setAuthCookies, setCookie } from "../src/helpers/cookie";
 import { withCookie } from 'next-cookie';
 import Header from "../src/components/consts/NotAuth/Header";
 
@@ -38,11 +37,18 @@ const Signup = (props) => {
     const googleProvider = new GoogleAuthProvider();
     const facebookProvider = new FacebookAuthProvider();
 
-    const dispatch = useDispatch();
+    const withGoogleService = () => {
+        signInWithPopup(firebaseAuth, googleProvider).catch(err => console.log('withGoogleService', err));
+    };
+
+    const withFacebookService = () => {
+        signInWithPopup(firebaseAuth, facebookProvider).catch(err => console.log('withFacebookService', err));
+    };
 
     const [password, setPassword] = useState('');
 
     const [formValidation, setFormValidation] = useState({
+        fullname: [],
         email: [],
         password: [],
         confirmPassword: [],
@@ -51,7 +57,7 @@ const Signup = (props) => {
     const [check1, setCheck1] = useState(false);
     const [check2, setCheck2] = useState(false);
 
-    const [validation, setValidation] = useState(true);
+    const [responseErrors, setResponseErrors] = useState([]);
 
     React.useEffect(() => {
         window.onbeforeunload = function (e) {
@@ -64,14 +70,37 @@ const Signup = (props) => {
         };
     }, []);
 
-    const handleChangeEmail = (email) => {
+    const handleChangeFullname = (fullname) => {
+        let validationErrors = [];
+
+        if (!fullname) {
+            validationErrors.push("Fullname field is required");
+        };
+
+        setFormValidation(prevState => {
+            return {
+                ...prevState,
+                fullname: validationErrors
+            };
+        });
+    };
+
+    const handleChangeEmail = async (email) => {
         let validationErrors = [];
 
         if (!email) {
             validationErrors.push("Email field is required");
-        };
-        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-            validationErrors.push("Email is not valid");
+        } else {
+            if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                validationErrors.push("Email is not valid");
+            } else {
+                const emailCheckFetch = await fetch(config.BASE_URL + `register/check?type=email&value=${email}`);
+                const checkEmail = await emailCheckFetch.json();
+
+                if (!checkEmail.success) {
+                    validationErrors.push(checkEmail.message);
+                };
+            };
         };
 
         setFormValidation(prevState => {
@@ -93,6 +122,9 @@ const Signup = (props) => {
         };
         if (password.length > 16) {
             validationErrors.push("Password must be maximum 16 characters");
+        };
+        if (!/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/.test(password)) {
+            validationErrors.push("Password is not valid");
         };
 
         setFormValidation(prevState => {
@@ -121,9 +153,33 @@ const Signup = (props) => {
         });
     };
 
-    useEffect(() => {
-        console.log('formValidation', formValidation);
-    }, [formValidation])
+    const checkFormValidation = (data) => {
+
+        const errorCount = 0;
+
+        for (const [key, value] of Object.entries(formValidation)) {
+            errorCount += value.length;
+        };
+
+        if (errorCount > 0) {
+            return false;
+        };
+
+        handleChangeFullname(data.full_name);
+        handleChangeEmail(data.email);
+        handleChangePassword(data.password);
+        handleChangeConfirmPassword(data.repeat_password);
+
+        for (const [key, value] of Object.entries(formValidation)) {
+            errorCount += value.length;
+        };
+
+        if (errorCount > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    };
 
     const signup_form = (event) => {
 
@@ -132,77 +188,39 @@ const Signup = (props) => {
 
         for (let [key, value] of data.entries()) {
             body[key] = value;
-        }
-
-        if (!body.password || !body.repeat_password) {
-            setValidation(false);
-            // return;
-        } else if (body.password.length < 8) {
-            setValidation(false);
-            toaster.push(<Notification type={"error"} header="Failed confirmation!" closable>
-                <p className="text-danger">Password must be at least 8 characters</p>
-            </Notification>, 'topEnd');
-            return;
-        } else if (body.password.length > 16) {
-            setValidation(false);
-            toaster.push(<Notification type={"error"} header="Failed confirmation!" closable>
-                <p className="text-danger">Password must be between 8 - 16 characters</p>
-            </Notification>, 'topEnd');
-            return;
         };
 
-        if (body.password !== body.repeat_password) {
-            setValidation(false)
-        } else {
-            !validation && setValidation(true);
+        const isValid = checkFormValidation(body);
 
-            axios.post(config.BASE_URL + "auth/register", {
-                email: body.email,
-                full_name: body.full_name,
-                password: body.password
-            }).then(res => {
-                let data = res.data.data;
+        if (!isValid) return;
 
-                cookie.remove('teammers-type');
+        axios.post(config.BASE_URL + "auth/register", {
+            email: body.email,
+            full_name: body.full_name,
+            password: body.password
+        }).then(res => {
 
-                // cookie.set('teammers-access-token', data.token);
-                // cookie.set('teammers-type', data.user.type);
-                setCookie('teammers-access-token', data.token, 6);
-                setCookie('teammers-type', data.user.type, 6);
-                router.push("/signup/steps");
-            })
-                .catch(error => {
-                    console.log('error signup', error);
+            console.log('signup form res', res);
 
-                    if (error.response?.status === 422) {
-                        toaster.push(
-                            <Notification type={"error"} header="Failed confirmation!" closable>
-                                {
-                                    renderErrorMessages(error.response.data.error.validation).map(item =>
-                                        <p className="text-danger">{item}</p>
-                                    )
-                                }
-                            </Notification>, 'topEnd'
-                        );
-                        return;
-                    }
-                    ;
+            if (res.data.success) {
+                setAuthCookies(res.data.data.token, res.data.data.user.full_name, res.data.data.user.type, res.data.data.user.id);
+            };
 
-                    // toaster.push(<Notification type={"error"} header="Failed confirmation!" closable>
-                    //     <p className="text-danger">{error.response?.data.error.message}</p>
-                    // </Notification>, 'topEnd');
-                })
-        }
-        // console.log(body)
+        }).catch(error => {
+            console.log('error signup', error);
+
+            if (error.response?.status === 422) {
+                let errors = renderErrorMessages(error.response.data.error.validation);
+                setResponseErrors(errors);
+
+                return;
+            };
+        });
     };
 
-    const withGoogleService = () => {
-        signInWithPopup(firebaseAuth, googleProvider).catch(err => console.log('withGoogleService', err));
-    };
-
-    const withFacebookService = () => {
-        signInWithPopup(firebaseAuth, facebookProvider).catch(err => console.log('withFacebookService', err));
-    };
+    // useEffect(() => {
+    //     console.log('formValidation', formValidation);
+    // }, [formValidation]);
 
     return (
         <div className="container">
@@ -275,13 +293,27 @@ const Signup = (props) => {
                                     <Form onSubmit={(condition, event) => {
                                         signup_form(event)
                                     }}>
-                                        <Form.Group controlId="full_name">
+                                        <Form.Group
+                                            controlId="full_name"
+                                            className={!formValidation.fullname.length ? '' : 'not-valid'}
+                                        >
                                             <Form.ControlLabel>Full Name</Form.ControlLabel>
                                             <Form.Control
                                                 name="full_name"
                                                 type="text"
                                                 placeholder="Full Name"
+                                                onBlur={e => handleChangeFullname(e.target.value)}
                                             />
+                                            {
+                                                formValidation.fullname.length > 0 &&
+                                                <div className="validation-errors">
+                                                    {
+                                                        formValidation.fullname.map((item, index) => {
+                                                            return <span key={index}>{item}</span>
+                                                        })
+                                                    }
+                                                </div>
+                                            }
                                         </Form.Group>
                                         <Form.Group
                                             controlId="email"
@@ -366,6 +398,13 @@ const Signup = (props) => {
                                                 Yes, I understand and agree to the Terms of Service, including the User Agreement and Privacy Policy.
                                             </Checkbox>
                                         </Form.Group>
+
+                                        {
+                                            responseErrors.map((item, index) => {
+                                                return <p key={index} className="text-danger">{item}</p>
+                                            })
+                                        }
+
                                         <Form.Group className="mt-5 mb-3">
                                             <ButtonToolbar>
                                                 <Button disabled={!check2} className="submit-btn" type="submit">Sign up</Button>
