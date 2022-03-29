c1773bc359476cccb0f29ca03319336d57cbaaf9
Asiman Qedirov <asiman.qadirov@a2z.az>
2022-03-29 17:50:33 +0400

    A

--- pages/owner/profile/settings.js
+++ pages/owner/profile/settings.js
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import BreadCrumb from '../../../src/components/Lib/BreadCrumb';
 import Banner from '../../../src/components/Lib/Banner';
-import { MdModeEdit, MdOutlineWorkOutline } from 'react-icons/md';
-import { RiSettingsLine } from 'react-icons/ri';
-import { FaRegTimesCircle } from 'react-icons/fa';
-import { Avatar, Button, Form } from 'rsuite';
-import { Cookie, withCookie } from 'next-cookie';
+import {MdModeEdit, MdOutlineWorkOutline} from 'react-icons/md';
+import {RiSettingsLine} from 'react-icons/ri';
+import {FaRegTimesCircle} from 'react-icons/fa';
+import {Avatar, Button, Form} from 'rsuite';
+import {Cookie, withCookie} from 'next-cookie';
 import config from '../../../src/configuration';
-import { getFetchData } from '../../../lib/fetchData';
-import { getToken } from '../../../lib/session';
+import {getFetchData} from '../../../lib/fetchData';
+import {getToken} from '../../../lib/session';
+import axios from "axios";
+import {getCookie} from "../../../src/helpers/cookie";
 
 function Setting(props) {
-
+    const [owner, setOwner] = useState({
+        project_role_id : '',
+        username: '',
+        email: '',
+        photo: '',
+        password: '',
+        new_password: ''
+    })
+    const [validation, setValidation] = useState(false)
+    useEffect(() => {
+        axios.get(config.BASE_URL + "auth/user?include=project,skills,positions,experiences,detail.location", {
+            headers: {
+                Authorization: "Bearer " + getCookie('teammers-access-token')
+            }
+        })
+            .then(res => {
+                console.log(res);
+                setOwner({
+                    ...owner,
+                    username: res.data?.data?.username,
+                    email: res.data?.data?.email,
+                    photo: res.data?.data?.detail?.photo
+                })
+            })
+    }, []);
+    const setData = (key, data) => {
+        setOwner({
+            ...owner,
+            [key]: data
+        })
+    }
+    const saveChanges = () => {
+        if ((!owner.password || !owner.new_password) && owner.password.trim() !== owner.new_password.trim()) {
+            setValidation(true)
+        } else {
+            axios.put(config.BASE_URL + "users", owner, {
+                headers: {
+                    Authorization: "Bearer " + getCookie('teammers-access-token')
+                }
+            })
+                .then(res => {
+                    console.log(res)
+                })
+            setValidation(false)
+        }
+    }
     return (
         <div className='teammer-profile-edit'>
-            <BreadCrumb />
+            <BreadCrumb/>
             <Banner
-                styles={{ marginBottom: '2.5rem' }}
+                styles={{marginBottom: '2.5rem'}}
             />
             <div className="profile-wrapper">
                 <div className="left-side">
@@ -25,7 +72,7 @@ function Setting(props) {
                         <li>
                             <Link href="/owner/profile/edit">
                                 <a>
-                                    <MdModeEdit />
+                                    <MdModeEdit/>
                                     <span>Edit Profile</span>
                                 </a>
                             </Link>
@@ -33,7 +80,7 @@ function Setting(props) {
                         <li className='active'>
                             <Link href="/owner/profile/settings">
                                 <a>
-                                    <RiSettingsLine />
+                                    <RiSettingsLine/>
                                     <span>Account Settings</span>
                                 </a>
                             </Link>
@@ -41,7 +88,7 @@ function Setting(props) {
                         <li>
                             <Link href="/owner/profile/company">
                                 <a>
-                                    <MdOutlineWorkOutline />
+                                    <MdOutlineWorkOutline/>
                                     <span>Company</span>
                                 </a>
                             </Link>
@@ -49,7 +96,7 @@ function Setting(props) {
                         <li>
                             <Link href="/owner/profile/blocked-users">
                                 <a>
-                                    <FaRegTimesCircle />
+                                    <FaRegTimesCircle/>
                                     <span>Blocked Users</span>
                                 </a>
                             </Link>
@@ -62,11 +109,11 @@ function Setting(props) {
                             <Avatar
                                 size="lg"
                                 circle
-                                src={props.userData?.detail?.photo ? props.userData.detail.photo : "https://www.w3schools.com/howto/img_avatar.png"}
+                                src={owner.photo ? owner.photo : "https://www.w3schools.com/howto/img_avatar.png"}
                                 alt="username surname"
                             />
                             <div className="profile-title-content">
-                                <h4>{props.userData?.full_name}</h4>
+                                <h4>{owner.full_name}</h4>
                                 <span>Edit Profile</span>
                             </div>
                         </div>
@@ -74,6 +121,7 @@ function Setting(props) {
                             color="blue"
                             appearance="primary"
                             className='save-btn'
+                            onClick={saveChanges}
                         >
                             Save Changes
                         </Button>
@@ -88,7 +136,8 @@ function Setting(props) {
                                         <Form.Control
                                             name="name"
                                             placeholder="Enter your username"
-                                            value={props.userData?.username}
+                                            value={owner.username}
+                                            onChange={(e) => setData('username', e)}
                                         />
                                     </Form.Group>
                                 </div>
@@ -99,7 +148,8 @@ function Setting(props) {
                                             name="email"
                                             type='email'
                                             placeholder="Enter your email"
-                                            value={props.userData?.email}
+                                            value={owner.email}
+                                            onChange={(e) => setData('email', e)}
                                         />
                                     </Form.Group>
                                 </div>
@@ -111,21 +161,28 @@ function Setting(props) {
                             <Form className="row">
                                 <p className='form-title'>Password</p>
                                 <div className="col-md-12 mb-4">
-                                    <Form.Group controlId="current_password">
-                                        <Form.ControlLabel>Current Password</Form.ControlLabel>
-                                        <Form.Control name="current_password" placeholder="Enter your current password" />
-                                    </Form.Group>
-                                </div>
-                                <div className="col-md-12 mb-4">
                                     <Form.Group controlId="new_password">
                                         <Form.ControlLabel>New Password</Form.ControlLabel>
-                                        <Form.Control name="new_password" placeholder="Enter your new password" />
+                                        <Form.Control
+                                            className={validation ? 'login-border-color' : ''}
+                                            name="new_password"
+                                            type="password"
+                                            value={owner.password} onChange={e => {
+                                            setData('password', e)
+                                        }} placeholder="Enter your new password"/>
                                     </Form.Group>
                                 </div>
                                 <div className="col-md-12">
                                     <Form.Group controlId="confirm_password">
                                         <Form.ControlLabel>Confirm Password</Form.ControlLabel>
-                                        <Form.Control name="confirm_password" placeholder="Enter your confirm password" />
+                                        <Form.Control className={validation ? 'login-border-color' : ''}
+                                                      name="confirm_password"
+                                                      onChange={(e) => {
+                                                          setData('new_password', e);
+                                                      }}
+                                                      type="password"
+                                                      value={owner.new_password}
+                                                      placeholder="Enter your confirm password"/>
                                     </Form.Group>
                                 </div>
                             </Form>
@@ -136,16 +193,6 @@ function Setting(props) {
         </div>
     )
 }
+
 Setting.layout = true;
 export default Setting;
-
-export const getServerSideProps = async (context) => {
-
-    const fetchUserInfo = await getFetchData("auth/user?include=skills,positions,experiences,detail.location", getToken(context));
-
-    return {
-        props: {
-            userData: fetchUserInfo?.data,
-        }
-    }
-};
