ccf617214d5d1e900414d3f614bfc14043cd1073
Asiman Qedirov <asiman.qadirov@a2z.az>
2022-04-05 14:36:35 +0400

    notification is ready

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,37 +1,132 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import config from '../src/configuration'
-import {Avatar} from "rsuite";
-
+import {Avatar, IconButton, Notification, Pagination, toaster} from "rsuite";
+import Image from 'next/image';
+import {HiArrowLeft} from "react-icons/hi";
+import {AiOutlineRight} from "react-icons/ai";
+import Link from 'next/link'
 const Notifications = () => {
-    const [notifications, setNotifications] = useState([])
-    useEffect(() => {
-        axios.get(config.BASE_URL + "users/notifications?per_page=4")
+    const [notifications, setNotifications] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [firstRender, setFirstRender] = useState(false);
+    const [activePage, setActivePage] = useState(1);
+    const getData = () => {
+        axios.get(config.BASE_URL + "users/notifications?page=" + activePage)
             .then(res => {
                 if (res.data.success) {
-                    if (res.data.data?.items) setNotifications(res.data.data.items)
+                    if (res.data.data?.items) setNotifications(res.data.data.items);
+                    setTotal(res.data.data.total);
                 }
             })
+    }
+    useEffect(() => {
+        getData();
     }, []);
+    const deleteNotification = (id) => {
+        axios.delete(config.BASE_URL + "users/notifications/" + id)
+            .then(() => {
+                getData();
+                toaster.push(
+                    <Notification type={"success"} header="Success!" closable>
+                       This message deleted successfly!
+                    </Notification>, 'topEnd'
+                );
+            })
+    }
+    const readNotification = (id) => {
+        axios.put(config.BASE_URL + "users/notifications/" + id + "/read")
+            .then(() => {
+                getData();
+                toaster.push(
+                    <Notification type={"success"} header="Success!" closable>
+                       This message has been read!
+                    </Notification>, 'topEnd'
+                );
+            })
+    }
+    useEffect(async () => {
+        if (firstRender) {
+            getData();
+        }
+        setFirstRender(true)
+    }, [activePage]);
+    console.log(notifications)
     return <div className="notifications">
+        <div className="breadcrumb-wrapper">
+           <Link href="/teammer/home" passHref>
+               <div className="goback-btn">
+                   <IconButton
+                       size="lg"
+                       icon={<HiArrowLeft />}
+                       className="goback-btn"
+                   />
+                   <span>Go back</span>
+               </div>
+           </Link>
+            <div className="custom-breadcrumb">
+                <span>Home</span>
+                <span className='breadcrumb-icon'>
+                            <AiOutlineRight />
+                        </span>
+                <span className='active'>
+                            Notifications
+                        </span>
+            </div>
+        </div>
+        <h4>Notifications</h4>
         <table>
             <thead>
             <tr>
                 <th>id</th>
                 <th>Notification</th>
                 <th>Date</th>
-                <th>Pinned to top</th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td><span className="active deactive"></span></td>
-                <td><Avatar circle
-                            src="https://www.w3schools.com/howto/img_avatar.png"/> <p>Denis Delton wants to add you to
-                    their Netflix team.</p></td>
-            </tr>
+            {
+                notifications.length > 0 ? notifications.map((item, index) => {
+                    return <tr key={index}>
+                        <td><span className={item.read_at ? "deactive" : "active"}
+                                  onClick={() => readNotification(item.id)}></span></td>
+                        <td><p className="d-flex justify-content-start align-items-center"><Avatar circle
+                                                                                                   src="https://www.w3schools.com/howto/img_avatar.png"/>
+                            <span className="ml-2 message">{
+                                item.data?.message
+                            }</span></p></td>
+                        <td><span>{item.created_at}</span></td>
+                        <td><span onClick={() => deleteNotification(item.id)}><IconButton
+                            size="sm"
+                            className='bg-transparent ml-2'
+                            icon={
+                                <Image
+                                    src={'/icons/trash.svg'}
+                                    alt='img'
+                                    width={16}
+                                    height={16}
+                                    layout='fixed'
+                                />
+                            }
+                        /></span></td>
+                    </tr>
+                }) : <p className="text-center">You have no yet any notification</p>
+            }
             </tbody>
         </table>
+        <div className="d-flex justify-content-end mt-3">
+            <Pagination
+                prev
+                last
+                next
+                first
+                size="xs"
+                total={48}
+                limit={10}
+                activePage={activePage}
+                onChangePage={setActivePage}
+            />
+        </div>
     </div>
 }
 Notifications.layout = true
